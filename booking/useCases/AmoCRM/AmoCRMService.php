2ab0ec8ff5e4c30f927dd193d559842ca0da636c
aleksey <busenov@ya.ru>
2024-03-04 10:41:18 +0300

    Отладка 1

--- booking/useCases/AmoCRM/AmoCRMService.php
+++ booking/useCases/AmoCRM/AmoCRMService.php
@@ -103,6 +103,64 @@ class AmoCRMService
         return true;
     }
 
+    public function longAuthorization():bool
+    {
+        $this->log(__FUNCTION__);
+        $link = 'https://' . $this->credential->domain . '/oauth2/access_token'; //Формируем URL для запроса
+        /** Формируем заголовки */
+        $headers = [
+            'Authorization: Bearer ' . $this->credential->token,
+        ];
+        /**
+         * Нам необходимо инициировать запрос к серверу.
+         * Воспользуемся библиотекой cURL (поставляется в составе PHP).
+         * Вы также можете использовать и кроссплатформенную программу cURL, если вы не программируете на PHP.
+         */
+        $curl = curl_init(); //Сохраняем дескриптор сеанса cURL
+        /** Устанавливаем необходимые опции для сеанса cURL  */
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-oAuth-client/1.0');
+        curl_setopt($curl,CURLOPT_URL, $link);
+        curl_setopt($curl,CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl,CURLOPT_HEADER, false);
+        curl_setopt($curl,CURLOPT_SSL_VERIFYPEER, 1);
+        curl_setopt($curl,CURLOPT_SSL_VERIFYHOST, 2);
+        $out = curl_exec($curl); //Инициируем запрос к API и сохраняем ответ в переменную
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+        /** Теперь мы можем обработать ответ, полученный от сервера. Это пример. Вы можете обработать данные своим способом. */
+        $code = (int)$code;
+        $errors = [
+            400 => 'Bad request',
+            401 => 'Unauthorized',
+            403 => 'Forbidden',
+            404 => 'Not found',
+            500 => 'Internal server error',
+            502 => 'Bad gateway',
+            503 => 'Service unavailable',
+        ];
+
+        try
+        {
+            /** Если код ответа не успешный - возвращаем сообщение об ошибке  */
+            if ($code < 200 || $code > 204) {
+                $response=json_decode($out, true);
+                dump($response);exit;
+                throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undefined error', $code);
+            }
+        } catch(\Exception $e)
+        {
+            die('Ошибка: ' . $e->getMessage() . PHP_EOL . 'Код ошибки: ' . $e->getCode());
+        }
+        /**
+         * Данные получаем в формате JSON, поэтому, для получения читаемых данных,
+         * нам придётся перевести ответ в формат, понятный PHP
+         */
+        $response = json_decode($out, true);
+        dump($response);exit;
+        $this->saveToken($response['access_token'],$response['refresh_token'],(time()+$response['expires_in']));
+        return true;
+    }
     public function simpleRefreshToken():bool
     {
         $this->log(__FUNCTION__);
